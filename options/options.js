85275b00c934e46a31242c85d821d13b7e284475
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 06:22:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-dream-dev-57118",
+  url: "https://flat-dream-dev-57118.botics.co",
+  api: "https://flat-dream-dev-57118.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
